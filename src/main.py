b57348180e72fb45b832d642e28ceadb17925fca
0x7070 <raykhelson.ielon@gmail.com>
2026-02-07 17:28:50 -0500

    sync

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from utils.general.helpers import *
 
 from utils.packets.feature_extraction import count, utc_stamp
 from utils.packets.handling import handle
-from utils.packets.time import time_between, time_to_date, time_to_iso, time
+from utils.packets.time import time_between, time_to_date, time_to_iso, time, format_duration_readable
 
 import pandas as pd
 from scapy.all import rdpcap
@@ -24,6 +24,7 @@ print(f'time of p0: {utc_stamp(p1)}')
 d_p = time_between(p0, p1)
 print(f'delta time: {d_p}')
 print(f'type delta time: {type(d_p)}')
+print(f'Converted time: {format_duration_readable(d_p)}')
 date = time_to_date(p0.time)
 print(f'Converted time `{type(date)}`')
 t = time(p0)
--- src/utils/packets/time.py
+++ src/utils/packets/time.py
@@ -1,10 +1,14 @@
 from scapy.all import Packet, EDecimal
 from scapy.layers.inet import TCP, UDP, ICMP
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 __all__ = [
-    '',
+    'time',
+    'time_between',
+    'time_to_date',
+    'time_to_iso',
+    'format_duration_readable'
 ]
 
 def time(pkt: Packet) -> EDecimal:
@@ -28,6 +32,18 @@ def time_to_iso(time: EDecimal): # ISO8601 time standard for logs
     ).isoformat()
     
 # TODO: Time to seconds, then time to human readable
+def format_duration_readable(seconds: EDecimal) -> str:
+    td = timedelta(seconds=float(seconds))
+    
+    total_seconds = int(td.total_seconds())
+    days, remainder = divmod(total_seconds, 86400) # total seconds in a day
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    
+    return f'{days}d {hours}h {minutes}m {seconds}s'
+
+def format_duration_seconds(time: EDecimal) -> str:
+    converted_time = # float time to format .2f seconds
 
 if __name__=='__main__':
     print(f'time module LOADED')
